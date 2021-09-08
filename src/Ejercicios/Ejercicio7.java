05c11493cf239426db0900279ea403d14f3d9df1
AngelG-JAPY <angelgarciara16@gmail.com>
2021-09-08 16:34:09 -0500

    Se terminaron los métodos importantes del ejercicio 7

--- src/Ejercicios/Ejercicio7.java
+++ src/Ejercicios/Ejercicio7.java
@@ -26,7 +26,7 @@ public class Ejercicio7 {
 
     private static final Scanner sc = new Scanner(System.in);
     private static int[] grades = new int[20];
-    private static int average = 0;
+    private static boolean order = false;
 
     /**
      * Pide los datos al usuario de manera manual.
@@ -43,6 +43,7 @@ public class Ejercicio7 {
      * @return promedio de tipo entero.
      */
     public static int gradesAverage() {
+        int average = 0;
 
         for (int i = 0; i < grades.length; i++) {
             average += grades[i];
@@ -58,6 +59,7 @@ public class Ejercicio7 {
      */
     public static void orderGrades() {
         grades = Arrays.orderArray_Bubble(grades);
+        order = true;
     }
 
     /**
@@ -89,7 +91,7 @@ public class Ejercicio7 {
      * @return -1 si el arreglo no esta ordenado. El total de elementos entre 18
      * y 20;
      */
-    public static int searchInRange(boolean order) {
+    public static int searchInRange() {
 
         int total = 0;
 
@@ -114,13 +116,14 @@ public class Ejercicio7 {
      * Determina cuantos elementos son mayores al promedio
      *
      * @param order indica si el arreglo ya esta ordenado en forma asc.
-     * @return El total de datos mayores al promedio si el arreglo esta ordenado. 
-     *  -1 Si el arreglo no esta ordenado.
-     *  -2 Si el promedio es igual 0.
+     * @return El total de datos mayores al promedio si el arreglo esta
+     * ordenado. -1 Si el arreglo no esta ordenado. -2 Si el promedio es igual
+     * 0.
      */
-    public static int higherThanAverage(boolean order) {
+    public static int higherThanAverage() {
         int total = 0;
         int average = gradesAverage();
+
         if (order && average != 0) {
             for (int i = grades.length - 1; i > -1; i--) {
                 if (grades[i] < average) {
@@ -137,8 +140,75 @@ public class Ejercicio7 {
         if (average == 0) {
             return -2;
         }
-        
+
         return total;
+    }
+
+    /**
+     * Determina cuantos estudiantes aprobaron. Para esto, el arreglo debe de
+     * andar ordenado.
+     *
+     * @return 100 si el primer elemento del arreglo es mayor a la nota minima.
+     * 0 si la ultima nota es menor a la nota minima. % de estudiantes aprovados
+     * en caso de no cumplirse ninguna. -1 si el arreglo no esta ordenado.
+     */
+    public static int approvedStudents() {
+
+        int approved = 0;
+
+        if (order) {
+            if (grades[0] >= 10) {
+                return 100;
+            } else if (grades[grades.length - 1] < 10) {
+                return 0;
+            } else {
+                for (int i = grades.length - 1; i > -1; i--) {
+                    if (grades[i] < 10) {
+                        break;
+                    }
+                    approved++;
+                }
+
+                return (approved * 100) / grades.length;
+
+            }
+        } else {
+            return -1;
+        }
+
+    }
+    
+    /**
+     * Determina cuantos estudiantes reprovaron.
+     * @return 100 si el ulitmo elemento del arreglo es menor a la nota minima.
+     * 0 si la primera nota es mayor a la nota minima. % de estudiantes reprovados
+     * en caso de no cumplirse ninguna. -1 si el arreglo no esta ordenado.
+     */
+
+    public static int failingStudents() {
+
+        int failing = 0;
+
+        if (order) {
+            if (grades[grades.length - 1] < 10) {
+                return 100;
+            } else if (grades[0] >= 10) {
+                return 0;
+            } else {
+                for (int i = grades.length - 1; i > -1; i--) {
+                    if (grades[i] < 10) {
+                        break;
+                    }
+                    failing++;
+                }
+
+                return (failing * 100) / grades.length;
+
+            }
+        } else {
+            return -1;
+        }
+
     }
 
 }
